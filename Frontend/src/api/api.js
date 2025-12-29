27a5950ec0361246d56e2cbe4c3e31ae764deb07
Ayan Manna <ayanmanna858@gmail.com>
2025-12-29 22:05:26 +0530

    url change

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://frostfault-api-chaos-simulator-sujk.vercel.app",
   headers: {
     "Content-Type": "application/json"
   }
