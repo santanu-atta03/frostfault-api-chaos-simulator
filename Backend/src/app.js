efc3cc1cc3644302e1e9694ee92df0940cbf5eeb
Ayan Manna <ayanmanna858@gmail.com>
2025-12-29 22:14:00 +0530

    not change

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,40 +11,18 @@ const errorHandler = require("./middlewares/error.middleware");
 const app = express();
 
 app.use(helmet());
-
-const allowedOrigins = [
-  "https://frostfault-api-chaos-simulator.vercel.app",
-  "http://localhost:5173"
-];
-
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    credentials: true
-  })
-);
-
-// 🔑 REQUIRED FOR BROWSERS
-app.options("*", cors());
-
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
 app.use("/api/mock", mockRoutes);
 app.use("/mock", serveMockRoutes);
 app.use("/api/analytics", analyticsRoutes);
+app.use(errorHandler);
+
 
 app.get("/", (req, res) => {
   res.json({ message: "API Chaos Engineering Simulator Backend is running 🚀" });
 });
-
-app.use(errorHandler);
 
 module.exports = app;
