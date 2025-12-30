4af1258ca2c1d4ffea3854f4804b39cac7d3b791
santanu-atta03 <santanuatta87@gmail.com>
2025-12-30 14:15:47 +0530

    frontend base url change

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://frostfault-api.onrender.com",
-  baseURL: "http://localhost:5000",
+  baseURL: "https://frostfault-api.onrender.com",
   headers: {
     "Content-Type": "application/json"
   }
